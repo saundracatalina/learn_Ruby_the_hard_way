be0d9904600e28c9ffb7761fb95763352700aa08
Saundra <saundracatalina@gmail.com>
2020-08-05 15:51:51 -0600

    Create file for lessons learned and things to research

---
+++ ex0.rb
@@ -0,0 +1,12 @@
+Things I've learned in Ruby!
+# if you put a pound sign in front of any line of code it knows not to run that line
+# In Ruby the " (double-quote) tells Ruby to replace variables it finds with #{}
+# but the ' (single-quote) tells Ruby to leave the string alone and ignore any variables inside it.
+# when printing a line you can use print or puts in the beginning of the line
+# puts is short for "put string"
+# the difference is that puts adds a new line after executing and print does not
+
+
+
+Things I don't get:
+# %{stuff} formatting with this
