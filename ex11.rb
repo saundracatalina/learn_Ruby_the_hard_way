f760e27356f4501d994476ca6cbafd376176f071
Saundra <saundracatalina@gmail.com>
2020-08-06 09:18:51 -0600

    Complete ex11 and experiment with gets.chomp

---
+++ ex11.rb
@@ -0,0 +1,18 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you in inches? "
+height = gets.chomp
+print "How much do you weigh in pounds? "
+weight = gets.chomp
+
+puts "So, you're #{age} years old, #{height} inches tall and weigh #{weight} pounds."
+
+# experimenting below with my own example
+print "What is your favorite genre of music? "
+genre = gets.chomp
+print "What is your favorite artist? "
+artist = gets.chomp
+print "What is your favorite song? "
+song = gets.chomp
+
+puts "What you're saying is that you like #{genre} music, your favorite artist is #{artist} and your favorite song is #{song}."
