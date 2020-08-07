e635652bf58a8c80e874b144bfa2887afc1bc74d
Saundra <saundracatalina@gmail.com>
2020-08-07 17:51:06 -0600

    Add notes of things learned

--- ex0.rb
+++ ex0.rb
@@ -8,8 +8,25 @@
 # if you put \n in front of text it makes it go on a new line and can be used mid-string
 # \t at the beginning of a string will tab it in
 # you can also put symbols after it like this
-tab_example = "\t* This line will be tabbed in, but also have an bullet point"
+tab_example = "\t* This line will be tabbed in, but also have a bullet point"
 puts tab_example
+
+vert_tab_example = "\vThis line will vertically tab between the last line and \vhere too."
+puts vert_tab_example
+
+# gets lets the user input a line and returns the value entered to your program as a string
+# if you just use gets then it includes a trailing line break
+# gets.chomp is how you cut off that line break that gets adds shown in ex11.rb
+# if you're asking at question that uses gets to store a response leave a space at the end of your string
+# like this "How old are you? " that way it comes out cleaner when running the program
+# ex13 shows how to use ARGV. to run it in the terminal you can enter something like this:
+# ruby ex13.rb blue green purple
+# that will fill in the first second and third variables from the command line
+# because ARGV is used you can't just use gets.chomp to ask for input while the script is running
+# you would need to use $stdin.gets.chomp so that an error doesn't get triggered
+# "If they give your script inputs on the command line, then you use ARGV."
+# "If you want them to input using the keyboard while the script is running, then use gets.chomp."
+# If you're running ARGV and want input while the script is running use $stdin.gets.chomp
 
 #Things I don't get:
 # %{stuff} formatting with this
